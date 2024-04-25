5f9d49cbbb25581d3c1dff8714a18e98c186b6b8
swaroop226 <45750205+swaroop226@users.noreply.github.com>
2024-04-25 18:15:44 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("im new to git")
+#Lucky number#
+a = b = c = d = (0,1,2,3,4,5,6,7,8,9)
+for i in a:
+  for j in b:
+    for k in c:
+      for l in d:
+        luck = a[i]+b[j]+c[k]+d[l]
+        if luck%9 == 0:
+          print(a[i],b[j],c[k],d[l])
